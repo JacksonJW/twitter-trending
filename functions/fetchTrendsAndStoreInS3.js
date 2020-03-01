ef88e23b39d61aea48f0f2ea84a5fdb6457edcde
JacksonJW <jacksonjwatkins@gmail.com>
2020-02-29 23:48:11 -0800

    Request to twitter

--- functions/fetchTrendsAndStoreInS3.js
+++ functions/fetchTrendsAndStoreInS3.js
@@ -1,15 +1,76 @@
 // TODO: implement fetchTrendsAndStoreInS3 function
 const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
-// const Twitter = require("twitter");
 const s3 = new AWS.S3();
+const { getTrends } = require("../helpers/twitter");
 
 module.exports.fetchTrendsAndStoreInS3 = async (event, context, callback) => {
-  const date = new Date();
-  const n = date.toISOString();
-  filename = `twitter-trends${n}.json`;
-  s3.putObject({
-    Bucket: process.env.BUCKET,
-    Key: filename,
-    Body: '{ "jackson":"watkins"}'
-  }).promise();
+  const dateTime = new Date().toISOString();
+  filename = `twitter-trends${dateTime}.json`;
+  getTrends() // eslint-disable-next-line consistent-return
+    .then(json => {
+      // console.log("JSON.stringify(json): " + JSON.stringify(json));
+      // console.log(
+      //   "typeof JSON.stringify(json): " + typeof JSON.stringify(json)
+      // );
+      // console.log("json[0]: " + json[0]);
+      console.log("json[0].trends: " + json[0].trends);
+      // console.log("Object.keys(json[0]): " + Object.keys(json[0]));
+      // console.log("json[0].status: " + json[0].status);
+      console.log(
+        "JSON.stringify(json[0].trends): " + JSON.stringify(json[0].trends)
+      );
+      // console.log("typeof json[0]: " + typeof json[0]);
+      // console.log("json[0][0]: " + json[0][0]);
+      // console.log("typeof json.status: " + typeof json.status);
+      // console.log("typeof json: " + typeof json);
+      console.log("Object.keys(json): " + Object.keys(json));
+      // console.log("json: " + json);
+      // console.log("json.status: " + json.status);
+      // console.log(
+      //   "Object.keys(json[0].trends): " + Object.keys(json[0].trends)
+      // );
+      console.log("json._headers.status[0]", json._headers.status[0]);
+
+      status = json._headers.status[0];
+
+      if (status !== "200 OK") {
+        return callback(null, {
+          statusCode: json.status,
+          body: JSON.stringify({
+            error: "Could not fetch twitter GET trends/place api"
+          })
+        });
+      }
+      console.log("test");
+      // Grab the list of trends object from the json
+      const { trends } = json[0];
+      console.log(`trends api response: ${trends}`);
+      return callback(null, {
+        statusCode: json._headers.status[0],
+        body: JSON.stringify(trends[0])
+      });
+
+      // Set up s3
+      // s3.putObject({
+      //   Bucket: process.env.BUCKET,
+      //   Key: filename,
+      //   Body: trends
+      // }).catch(err => {
+      //   callback(err, null);
+      // });
+    })
+    .catch(error => {
+      callback(null, { statusCode: 500, body: JSON.stringify({ error }) });
+    });
+
+  //   fetch("image URL")
+  //     .then(res => {
+  //       return s3.putObject({ Bucket, Key, Body: res.body }).promise();
+  //     })
+  //     .then(res => {
+  //       callback(null, res);
+  //     })
+  //     .catch(err => {
+  //       callback(err, null);
+  //     });
 };
---
+++ helpers/twitter.js
@@ -0,0 +1,32 @@
+require("dotenv").config({ path: __dirname + "/../.env" });
+const Twitter = require("twitter-lite");
+
+let twitterClient = new Twitter({
+  subdomain: "api",
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+// account verification
+// twitterClient
+//   .get("account/verify_credentials")
+//   .then(results => {
+//     console.log("results", results);
+//   })
+//   .catch(console.error);
+const woeId = "2352824";
+const params = { id: woeId };
+
+const getTrends = () =>
+  new Promise((resolve, reject) =>
+    twitterClient
+      .get("trends/place", params)
+      .then(response => resolve(response))
+      .catch(error => reject(error))
+  );
+
+module.exports = {
+  getTrends
+};
