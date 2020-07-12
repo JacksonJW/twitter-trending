98306d3f33c6593fe487937071bd8d1b20b3b757
JacksonJW <jacksonjwatkins@gmail.com>
2020-07-11 20:08:01 -0700

    Set up trendsCounter

--- functions/processTrendsAndSend.js
+++ functions/processTrendsAndSend.js
@@ -1,11 +1,23 @@
 // TODO: implement processTrendsAndSend function
 const sgMail = require("@sendgrid/mail");
 const moment = require("moment");
+const { listS3Files } = require("../helpers/s3");
 
 module.exports.processTrendsAndSend = (event, context, callback) => {
   const formattedDate = moment().format("dddd MMMM Do, YYYY"); // ex. Saturday April 25th, 2020
-
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  // let trendsCounter = {};
+
+  listS3Files()
+    .then((response) => {
+      // response.Content.forEach((trend) => {
+      //   trendsCounter[trend]
+      // });
+      console.log(response.Content);
+    })
+    .catch((error) => callback(error, null));
+
+  // Send email
   const msg = {
     to: "jacksonjwatkins@gmail.com",
     from: "jacksonjwatkins@gmail.com",
